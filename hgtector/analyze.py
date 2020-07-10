9ce1fa9e4959b8a3273d5a715b91f868fe8145b6
surh <sur00mx@gmail.com>
2020-07-10 11:59:04 -0700

    homogenizing coding style

--- hgtector/analyze.py
+++ hgtector/analyze.py
@@ -276,7 +276,7 @@ class Analyze(object):
         data = []
         with read_file(file) as f:
             for line in f:
-                line = line.rstrip("\n")
+                line = line.rstrip('\r\n')
                 m = p.match(line)
                 if m:
                     if m.group(1) == 'ID':
